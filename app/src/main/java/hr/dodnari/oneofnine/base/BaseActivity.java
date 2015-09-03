dcc177e6bfeab70302c38cae21f1667819fb81e5
Marko Filipovic <mrkfilipovic3@gmail.com>
2015-09-03 16:10:48 +0200

    added ActionBarDrawerToggle

--- app/src/main/java/hr/dodnari/oneofnine/base/BaseActivity.java
+++ app/src/main/java/hr/dodnari/oneofnine/base/BaseActivity.java
@@ -1,12 +1,16 @@
 package hr.dodnari.oneofnine.base;
 
+import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import butterknife.Bind;
@@ -18,6 +22,7 @@ import hr.dodnari.oneofnine.R;
  */
 public class BaseActivity extends AppCompatActivity {
 
+    private static final String TAG = BaseActivity.class.getSimpleName();
     @Bind(R.id.toolbar)
     protected Toolbar toolbar;
 
@@ -25,11 +30,11 @@ public class BaseActivity extends AppCompatActivity {
     protected DrawerLayout drawerLayout;
 
     private ActionBar actionBar;
+    private ActionBarDrawerToggle drawerToggle;
     private int activityTitle;
 
     @Override
     public void setContentView(int layoutResID) {
-        super.setContentView(layoutResID);
         DrawerLayout fullView = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_base, null);
         FrameLayout activityContainer = (FrameLayout) fullView.findViewById(R.id.activity_content);
         getLayoutInflater().inflate(layoutResID, activityContainer, true);
@@ -37,6 +42,21 @@ public class BaseActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setupToolbar();
+        setupNavigationDrawer();
+    }
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+
+    private void setupNavigationDrawer() {
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(drawerToggle);
     }
 
     private void setupToolbar() {
@@ -44,31 +64,15 @@ public class BaseActivity extends AppCompatActivity {
         setTitle(activityTitle);
 
         actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setShowHideAnimationEnabled(true);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setShowHideAnimationEnabled(true);
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                openDrawer();
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    // TODO replace with ActionBarDrawerToggle
-    private void openDrawer() {
-        drawerLayout.openDrawer(Gravity.LEFT);
+        return drawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 
     protected void setActivityTitle(int activityTitle) {
